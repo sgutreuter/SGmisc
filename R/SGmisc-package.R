98c8541ff2aa10fe196c476444e5907a3f6357d7
Steve Gutreuter <sgutreuter@gmail.com>
2020-03-26 18:49:08 -0400

    Added SGmisc-package.R

---
+++ R/SGmisc-package.R
@@ -0,0 +1,9 @@
+#' SGmisc
+#'
+#' A collection of trivially small wrappers and convienience functions.
+#'
+#' @name SGmisc
+#' @docType package
+#' @author Steve Gutreuter: \email{sgutreuter@@gmail.com}
+#'
+NULL
